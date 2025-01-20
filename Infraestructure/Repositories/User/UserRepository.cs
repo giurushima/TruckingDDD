459042ead6c2485dc22ca21d9dca0bc7e8a06e62
Gabriel <giurushima@hotmail.com>
2025-01-20 20:39:47 -0300

    ADD: Agregado de repository users para auth y applicationcontext.

---
+++ Infraestructure/Repositories/User/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories.User
+{
+    public class UsersRepository : IUsersRepository
+    {
+        internal readonly ApplicationContext _context;
+
+        public UsersRepository(ApplicationContext context)
+        {
+            this._context = context;
+        }
+
+        public User? ValidateUser(AuthenticationRequestBody authRequestBody)
+        {
+            return _context.Users.FirstOrDefault(p => p.UserName == authRequestBody.UserName && p.Password == authRequestBody.Password);
+        }
+
+        public bool SaveChange()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
