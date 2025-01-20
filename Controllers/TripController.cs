c782fc0649d2dced56814c521f885460e07b2d21
Gabriel <giurushima@hotmail.com>
2025-01-20 20:19:14 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruckingDDD.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/TruckerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruckingDDD.Controllers
+{
+    public class TruckerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
