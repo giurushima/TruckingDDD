ac3879a609ac534b2882eb072a8124ac2f4ace88
Gabriel <giurushima@hotmail.com>
2025-01-20 20:38:39 -0300

    ADD: Carpeta y archivo para repository de autenticacion JWT.

---
+++ Infraestructure/Repositories/Auth/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories.Auth
+{
+    public class AuthenticationServices : IAuthenticationServices
+    {
+        private readonly IUsersRepository _userRepository;
+
+        public AuthenticationServices(IUsersRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User? ValidateUser(AuthenticationRequestBody authenticationRequest)
+        {
+            if (string.IsNullOrEmpty(authenticationRequest.UserName) || string.IsNullOrEmpty(authenticationRequest.Password))
+                return null;
+
+            return _userRepository.ValidateUser(authenticationRequest);
+        }
+    }
+}
